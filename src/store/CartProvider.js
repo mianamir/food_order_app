9356b2393b7b29ca133dc87b4c903a67a2a0b556
Muhammad Amir <mianamirlahore@gmail.com>
2022-03-08 16:34:00 +0500

    added cart reducer for handling the complex state of cart items.
    added cart reducer for handling the complex state of cart items.

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,17 +1,52 @@
+import { useReducer } from "react"; // due to complex state using this
 import CartContext from "./cart.context";
 
 
+const defaultCartState = {
+    items: [],
+    totalAmount: 0
+};
+
+const cartReducer = (state, action) => {
+
+    if (action.type === 'ADD'){
+        const updatedItems = state.items.contact(action.item);
+        const updatedTotalAmount = state.totalAmount + (action.item.price * action.item.amount);
+        
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        };
+    }
+
+    return defaultCartState;
+};
+
 const CartProvider = (props) => {
 
-    const addItemToCartHandler = items => {};
+    const [cartState, dispatchCartAction] = useReducer(cartReducer, defaultCartState);
 
-    const removeItemFromCartHandler = id => {};
+
+
+    const addItemToCartHandler = item => {
+        dispatchCartAction({
+            type: 'ADD', 
+            item: item
+        });
+    };
+
+    const removeItemFromCartHandler = id => {
+        dispatchCartAction({
+            type: 'REMOVE',
+            id: id
+        });
+    };
 
 
 
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: cartState.items,
+        totalAmount: cartState.totalAmount,
         addItem:addItemToCartHandler,
         removeItem:removeItemFromCartHandler
     };
