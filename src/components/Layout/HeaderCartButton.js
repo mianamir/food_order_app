996a6cb6a143cec915bc0654c43311542a09e248
Muhammad Amir <mianamirlahore@gmail.com>
2022-03-12 00:37:10 +0500

    cart button dynamic effects and Dockerfille added
    cart button dynamic effects and Dockerfille added.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
@@ -7,18 +7,39 @@ import classes from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
 
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
 
     // when CartContext will be updated this component will be re-evaluated 
     const cartCtx = useContext(CartContext); // established the connection with context
 
+    const { items } = cartCtx;
     
     // calculate the cart items total from context cart items
-    const numberOfCartItems = cartCtx.items.reduce( (currentNum, item) => {
+    const numberOfCartItems = items.reduce( (currentNum, item) => {
         return currentNum + item.amount;
     }, 0);
 
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump: ''}`;
 
-    return <button className={classes.button} onClick={props.onClick}>
+    useEffect(() => {
+        if( items.length === 0 ){
+            return;
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        // cleanup function
+        return () => {
+            clearTimeout(timer);
+        }
+ 
+    }, [items]);
+
+
+    return <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
             
